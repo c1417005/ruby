337ae7266c9c7ec020e13537604b2a4aacbba44e
SO FUKUSHIMA <c1417005@st.kanazawa-it.ac.jp>
2026-05-30 14:40:47 +0000

    map

---
+++ ruby_projects/map.rb
@@ -0,0 +1,28 @@
+# numbers = [1,2,3,4,5]
+# new_numbers = []
+
+# numbers.each do |n|
+#   new_numbers << n * 2
+
+# end
+
+# puts new_numbers
+
+# numbers = [1,2,3,4,5]
+
+# new_numbers =  numbers.map do |n|
+#   n*2
+# end
+
+# puts new_numbers
+
+
+numbers_2 = [10,20,30,40,50]
+new_numbers_2 = []
+
+numbers_2.each do |n|
+  new_numbers_2 << n*2
+end
+
+puts new_numbers_2
+
